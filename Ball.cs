f0e2414c09be27b1d8f61467d43cb43580dd7847
Gustav-Joestar <149873362+Gustav-Joestar@users.noreply.github.com>
2023-11-26 15:26:57 +0300

    Add files via upload

---
+++ Ball.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing.Drawing2D;
+using System.Windows;
+using System.Data;
+
+namespace Balls
+{
+    internal class Ball
+    {
+
+        public int x, y;
+        public int velocityX, velocityY;
+        public int accelX, accelY;
+        public int rad;
+        public bool isMoving;
+        public int WIDTH, HEIGHT;
+        public Color color;
+
+
+        public Ball(int x, int y, int speedX, int speedY, int accelX, int accelY, int radius, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.velocityX = speedX;
+            this.velocityY = speedY;
+            this.rad = radius;
+            this.accelX = accelX;
+            this.accelY = accelY;
+            this.color = color;
+        }
+
+
+        public void setEnv(bool isMoving, int width, int height)
+        {
+            this.isMoving = isMoving;
+            this.WIDTH = width;
+            this.HEIGHT = height;
+        }
+
+
+        public void move(int n, Form form)
+        {
+            
+
+            while (isMoving)
+            {
+
+                velocityX += accelX;
+                velocityY += accelY;
+
+
+                x += velocityX;
+                y += velocityY;
+
+
+                if (x + rad > WIDTH || x - rad < 0)
+                {
+                    velocityX = -velocityX;
+                }
+                if (y + rad > HEIGHT || y - rad < 0)
+                {
+                    velocityY = -velocityY;
+                }
+
+
+                form.Invalidate();
+
+
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Balls
+{
+    public partial class Form1 : Form
+    {
+
+        private const int ballsCntr = 2;
+        private Ball[] balls = new Ball[ballsCntr];
+        private Thread[] threads = new Thread[ballsCntr];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            this.DoubleBuffered = true;
+
+
+            Random rnd = new Random();
+            for (int i = 0; i < balls.Length; i++)
+            {
+                Color color = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+                balls[i] = new Ball(rnd.Next(100, ClientSize.Width - 100), rnd.Next(100, ClientSize.Height - 100),
+                                    rnd.Next(1, 5), rnd.Next(1, 5),
+                                    0, 0, 100, color);
+                balls[i].setEnv(true, ClientSize.Width, ClientSize.Height);
+                int index = i;
+                threads[i] = new Thread(() => balls[index].move(index, this));
+            }
+
+            foreach (Thread t in threads)
+            {
+                t.Start();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Brush brush;
+
+            for (int i = 0; i < balls.Length; i++)
+            {
+                brush = new SolidBrush(balls[i].color);
+
+                e.Graphics.FillEllipse(brush, balls[i].x - balls[i].rad,
+                                              balls[i].y - balls[i].rad,
+                                              2 * balls[i].rad,
+                                              2 * balls[i].rad);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            foreach (Ball b in balls)
+            {
+                b.isMoving = false;
+            }
+
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
